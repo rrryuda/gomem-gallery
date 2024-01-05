8a031e7116ad3a5d9bd0c5d883ccdcd4369665c1
seunghyun <shawnyoo.des@gmail.com>
2024-01-06 02:44:31 +0900

    refactoring
    1. add bio
    2. refactoring

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Animation */
 window.onload = function () {
     var pictureElements = document.querySelectorAll("main picture");
 
@@ -8,3 +9,25 @@ window.onload = function () {
         }, (index + 2) * 150);
     });
 };
+
+/* Bio */
+function showPopup(event) {
+    var popup = document.getElementById("bio");
+    popup.style.display = "block";
+    updatePopupPosition(event);
+    window.addEventListener("mousemove", updatePopupPosition);
+}
+
+function hidePopup() {
+    var popup = document.getElementById("bio");
+    popup.style.display = "none";
+    window.removeEventListener("mousemove", updatePopupPosition);
+}
+
+function updatePopupPosition(event) {
+    var popup = document.getElementById("bio");
+    var x = event.clientX + 10;
+    var y = event.clientY + 10;
+    popup.style.left = x + "px";
+    popup.style.top = y + "px";
+}
